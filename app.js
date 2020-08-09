545666afd9dd60637dd6e08162a2b894bbef4fe7
RobBl8ke <RobB@korbicom.com>
2020-08-09 10:19:28 +0200

    Common admin route segment

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 
 
 // Your routers
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -4,12 +4,14 @@ const router = express.Router();
 module.exports = router;
 
 
+// GET
 router.get('/add-product', (req, res, next) => {
     console.log('Navigated to... Add Product!');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Products</button></form>');
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Products</button></form>');
 });
 
-router.post('/product', (req, res, next) => {
+// POST
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
