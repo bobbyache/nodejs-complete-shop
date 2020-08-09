fd1db5aac8a19caa1a290ad4c3c8f4f63a4bd29c
RobBl8ke <RobB@korbicom.com>
2020-08-09 10:47:06 +0200

    Serving up html views and working with path

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
@@ -6,8 +7,7 @@ module.exports = router;
 
 // GET
 router.get('/add-product', (req, res, next) => {
-    console.log('Navigated to... Add Product!');
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Products</button></form>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 // POST
--- routes/shop.js
+++ routes/shop.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
@@ -5,5 +6,5 @@ module.exports = router;
 
 router.get('/', (req, res, next) => {
     console.log('In another middleware!');
-    res.send('<h1>Hello from Express</h1>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
